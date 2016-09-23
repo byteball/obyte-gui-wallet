29e3359c62890283281277109e0dc7da614ccaca
tony <tonyofbyteball@users.noreply.github.com>
2016-09-23 17:05:21 +0300

    tabs

--- src/js/controllers/acceptCorrespondentInvitation.js
+++ src/js/controllers/acceptCorrespondentInvitation.js
@@ -2,73 +2,73 @@
 
 angular.module('copayApp.controllers').controller('acceptCorrespondentInvitationController',
   function($scope, $rootScope, $timeout, configService, profileService, isCordova, go, correspondentListService) {
-    
-    var self = this;
-    console.log("acceptCorrespondentInvitationController");
-    
-    var fc = profileService.focusedClient;
-      $scope.backgroundColor = fc.backgroundColor;
-    
-      $scope.beforeQrCodeScan = function() {
-          console.log("beforeQrCodeScan");
-        $scope.error = null;
-      };
-    
-      $scope.onQrCodeScanned = function(data, pairingCodeForm) {
-          console.log("onQrCodeScanned", data);
-          data = data.replace('byteball:', '');
-          handleCode(data);
-      };
-    
+	
+	var self = this;
+	console.log("acceptCorrespondentInvitationController");
+	
+	var fc = profileService.focusedClient;
+	$scope.backgroundColor = fc.backgroundColor;
+	
+	$scope.beforeQrCodeScan = function() {
+		console.log("beforeQrCodeScan");
+		$scope.error = null;
+	};
 
-      $scope.pair = function() {
-        $scope.error = null;
-          handleCode($scope.code);
-      };
+	$scope.onQrCodeScanned = function(data, pairingCodeForm) {
+		console.log("onQrCodeScanned", data);
+		data = data.replace('byteball:', '');
+		handleCode(data);
+	};
 
-    function handleCode(code){
-        var matches = code.match(/^([\w\/+]+)@([\w.:\/-]+)#([\w\/+]+)$/);
-        if (!matches)
-            return setError("Invalid pairing code");
-        var pubkey = matches[1];
-        var hub = matches[2];
-        var pairing_secret = matches[3];
-        if (pubkey.length !== 44)
-            return setError("Invalid pubkey length");
-        //if (pairing_secret.length !== 12)
-        //    return setError("Invalid pairing secret length");
-        console.log(pubkey, hub, pairing_secret);
-        self.setOngoingProcess("pairing");
-        correspondentListService.acceptInvitation(hub, pubkey, pairing_secret, function(err){
-            self.setOngoingProcess();
-            // acceptInvitation() will already open chat window
-            /*if (err)
-                $scope.error = err;
-            else
-                go.path('correspondentDevices');*/
-        });
-    }
-    
-    function setError(error){
-        $scope.error = error;
-    }
 
-  this.setOngoingProcess = function(name) {
-    var self = this;
+	$scope.pair = function() {
+		$scope.error = null;
+		handleCode($scope.code);
+	};
 
-    if (isCordova) {
-      if (name) {
-        window.plugins.spinnerDialog.hide();
-        window.plugins.spinnerDialog.show(null, name + '...', true);
-      } else {
-        window.plugins.spinnerDialog.hide();
-      }
-    } else {
-        $scope.onGoingProcess = name;
-      $timeout(function() {
-        $rootScope.$apply();
-      });
-    };
-  };
-    
+	function handleCode(code){
+		var matches = code.match(/^([\w\/+]+)@([\w.:\/-]+)#([\w\/+]+)$/);
+		if (!matches)
+			return setError("Invalid pairing code");
+		var pubkey = matches[1];
+		var hub = matches[2];
+		var pairing_secret = matches[3];
+		if (pubkey.length !== 44)
+			return setError("Invalid pubkey length");
+		//if (pairing_secret.length !== 12)
+		//    return setError("Invalid pairing secret length");
+		console.log(pubkey, hub, pairing_secret);
+		self.setOngoingProcess("pairing");
+		correspondentListService.acceptInvitation(hub, pubkey, pairing_secret, function(err){
+			self.setOngoingProcess();
+			// acceptInvitation() will already open chat window
+			/*if (err)
+				$scope.error = err;
+			else
+				go.path('correspondentDevices');*/
+		});
+	}
+	
+	function setError(error){
+		$scope.error = error;
+	}
+
+	this.setOngoingProcess = function(name) {
+		var self = this;
+
+		if (isCordova) {
+			if (name) {
+				window.plugins.spinnerDialog.hide();
+				window.plugins.spinnerDialog.show(null, name + '...', true);
+			} else {
+				window.plugins.spinnerDialog.hide();
+			}
+		} else {
+			$scope.onGoingProcess = name;
+			$timeout(function() {
+				$rootScope.$apply();
+			});
+		};
+	};
+	
   });
