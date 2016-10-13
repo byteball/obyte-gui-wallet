f4d4a46cff06271ce5ff4392438fefffe3173179
kakysha <kakysha@me.com>
2016-10-13 23:24:15 +0300

    send on enter in chat

--- src/js/controllers/correspondentDevice.js
+++ src/js/controllers/correspondentDevice.js
@@ -266,4 +266,15 @@ angular.module('copayApp.controllers').controller('correspondentDeviceController
 			});
 		}
 	};
-});
+}).directive('ngEnter', function() {
+    return function(scope, element, attrs) {
+        element.bind("keydown", function(e) {
+            if(e.which === 13 && !e.shiftKey) {
+                scope.$apply(function(){
+                    scope.$eval(attrs.ngEnter, {'e': e});
+                });
+                e.preventDefault();
+            }
+        });
+    };
+});;
