5418e07c35d785340e8d42e0693df2f3bc155e3d
kakysha <kakysha@me.com>
2016-10-14 21:48:12 +0300

    Badge new messages count

--- src/js/controllers/correspondentDevice.js
+++ src/js/controllers/correspondentDevice.js
@@ -7,6 +7,7 @@ angular.module('copayApp.controllers').controller('correspondentDeviceController
   function($scope, $rootScope, $timeout, $sce, $modal, configService, profileService, animationService, isCordova, go, correspondentListService, lodash) {
 	
 	var self = this;
+	var win = nw.Window.get();
 	console.log("correspondentDeviceController");
 	
 	var fc = profileService.focusedClient;
@@ -84,7 +85,29 @@ angular.module('copayApp.controllers').controller('correspondentDeviceController
 		go.path('editCorrespondentDevice');
 	};
 
-	
+	$scope.stopCountNewMessages = function() {
+		$scope.newMessagesCount=0;
+		$scope.counterEnabled = false;
+	}
+	$scope.stopCountNewMessages();
+	win.on('focus', function(){$scope.stopCountNewMessages();$scope.$apply();});
+	win.on('blur', function(){$scope.counterEnabled = true;});
+	$scope.$watch('newMessagesCount', function(count) {
+		if (count) {
+			win.setBadgeLabel(""+count);
+		} else {
+			win.setBadgeLabel("");
+		}
+	});
+	$scope.$watchCollection('messageEvents', function (newMessages, oldMessages) {
+		if (!$scope.counterEnabled) return;
+		var diffArray = lodash.difference(newMessages, oldMessages)
+		if (diffArray.length)
+			for (var i in diffArray) {
+				if (diffArray[i].bIncoming) $scope.newMessagesCount++;
+			}
+	});
+
 	function setError(error){
 		console.log("send error:", error);
 		$scope.error = error;
