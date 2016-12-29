08dc5d07fd17863b4104399687d3d633c8640493
tony <tonyofbyteball@users.noreply.github.com>
2016-12-29 13:28:11 +0300

    show version, testnet link

--- src/js/controllers/newVersionIsAvailable.js
+++ src/js/controllers/newVersionIsAvailable.js
@@ -2,10 +2,14 @@
 
 angular.module('copayApp.controllers').controller('newVersionIsAvailable', function($scope, $modalInstance, go, newVersion){
 
+  $scope.version = newVersion.version;
+	
   $scope.openDownloadLink = function(){
     var link = '';
     if (navigator && navigator.app) {
-      link = 'https://play.google.com/store/apps/details?id=org.byteball.wallet'
+      link = 'https://play.google.com/store/apps/details?id=org.byteball.wallet';
+	  if (newVersion.version.match('t$'))
+		  link += '.testnet';
     }
     else {
       link = 'https://github.com/byteball/byteball/releases/tag/v' + newVersion.version;
