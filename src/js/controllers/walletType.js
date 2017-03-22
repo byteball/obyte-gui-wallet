987afb196921596c8391503f76d546c430a0486b
vakar888 <vakar@aikq.uk>
2017-03-22 15:36:32 +0100

    wallet type right of version

--- src/js/controllers/walletType.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('copayApp.controllers').controller('walletTypeController', function() {
-  var conf = require('byteballcore/conf.js');
-  this.type = (conf.bLight ? 'light wallet' : 'full wallet');
-});
