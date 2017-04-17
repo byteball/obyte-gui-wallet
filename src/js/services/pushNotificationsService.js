43648e0aa901ff13dd9a0f462bc778eab13bf818
xJeneKx <xxjenekxx@gmail.com>
2017-04-17 11:22:45 +0300

    fixes push notifications

--- src/js/services/pushNotificationsService.js
+++ src/js/services/pushNotificationsService.js
@@ -17,94 +17,72 @@ angular.module('copayApp.services')
 		});
 	}
 	
+	window.onNotification = function(data) {
+		if (data.event === 'registered') {
+			storageService.setPushInfo(projectNumber, data.regid, true, function() {
+				sendRequestEnableNotification(_ws, data.regid);
+				$rootScope.$emit('Local/pushNotificationsReady');
+			});
+		}
+		else {
+			return false;
+		}
+	};
+	
 	eventBus.on('receivedPushProjectNumber', function(ws, data) {
 		_ws = ws;
 		if (data && data.projectNumber !== undefined) {
 			storageService.getPushInfo(function(err, pushInfo) {
-				projectNumber = data.projectNumber;
-				if (pushInfo && projectNumber == 0) {
+				var config = configService.getSync();
+				projectNumber = data.projectNumber + "";
+				if (pushInfo && projectNumber === "0") {
 					root.pushNotificationsUnregister(function() {
 						
 					});
 				}
-				else if (pushInfo && pushInfo.projectNumber && pushInfo.projectNumber === projectNumber) {
-					if (pushInfo.enabled) {
-						sendRequestEnableNotification(ws, pushInfo.registrationId);
-					}
-					$rootScope.$emit('Local/pushNotificationsReady');
-				}
-				else if (projectNumber) {
-					root.pushNotificationsInit(function(registrationId) {
-						sendRequestEnableNotification(ws, registrationId);
-						$rootScope.$emit('Local/pushNotificationsReady');
-					});
+				else if (projectNumber && config.pushNotifications.enabled) {
+					root.pushNotificationsInit();
 				}
 			});
 		}
 	});
 	
-	root.pushNotificationsInit = function(cb) {
+	root.pushNotificationsInit = function() {
 		if (!usePushNotifications) return;
 		
-		var config = configService.getSync();
-		if (!config.pushNotifications.enabled) return;
-		
-		defaults.pushNotifications.config.android.senderID = projectNumber;
-		
-		var push = PushNotification.init(defaults.pushNotifications.config);
-		
-		push.on('registration', function(data) {
-			storageService.setPushInfo(projectNumber, data.registrationId, true, function() {
-				cb(data.registrationId);
+		window.plugins.pushNotification.register(function(data) {
+			},
+			function(e) {
+				alert('err= ' + e);
+			}, {
+				"senderID": projectNumber,
+				"ecb": "onNotification"
 			});
-		});
-		push.on('notification', function(data) {
-			
-		});
 		
-		push.on('error', function(e) {
-			alert(JSON.stringify(e));
+		configService.set({pushNotifications: {enabled: true}}, function(err) {
+			if (err) $log.debug(err);
 		});
 	};
 	
-	root.pushNotificationsUnregister = function(cb) {
-		if (!usePushNotifications) return;
-		
-		var config = configService.getSync();
-		if (!config.pushNotifications.enabled) return;
-		
-		defaults.pushNotifications.config.android.senderID = projectNumber;
-		
-		var push = PushNotification.init(defaults.pushNotifications.config);
-		push.unregister(function() {
-			cb()
-		}, function() {
-			cb();
-		});
-	};
-	
-	root.enableNotifications = function() {
-		if (!usePushNotifications) return;
-		
-		var config = configService.getSync();
-		if (!config.pushNotifications.enabled) return;
-		
+	function disable_notification() {
 		storageService.getPushInfo(function(err, pushInfo) {
-			storageService.setPushInfo(pushInfo.projectNumber, pushInfo.registrationId, true, function() {
-				sendRequestEnableNotification(_ws, pushInfo.registrationId);
-			});
-		});
-	};
-	
-	root.disableNotifications = function() {
-		if (!usePushNotifications) return;
-		
-		storageService.getPushInfo(function(err, pushInfo) {
-			storageService.setPushInfo(pushInfo.projectNumber, pushInfo.registrationId, false, function() {
+			storageService.removePushInfo(function() {
 				network.sendRequest(_ws, 'hub/disable_notification', pushInfo.registrationId, false, function(ws, request, response) {
 					if (!response || (response && response !== 'ok')) return $log.error('Error sending push info');
 				});
 			});
+		});
+		configService.set({pushNotifications: {enabled: false}}, function(err) {
+			if (err) $log.debug(err);
+		});
+	}
+	
+	root.pushNotificationsUnregister = function() {
+		if (!usePushNotifications) return;
+		window.plugins.pushNotification.unregister(function() {
+			disable_notification();
+		}, function() {
+			disable_notification();
 		});
 	};
 	
