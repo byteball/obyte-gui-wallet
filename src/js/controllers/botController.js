1ec4b227423d0ff759449e4ae84f0455f244ca91
kakysha <kakysha@me.com>
2017-06-01 17:45:50 +0300

    removed directive copy

--- src/js/controllers/botController.js
+++ src/js/controllers/botController.js
@@ -35,15 +35,4 @@ angular.module('copayApp.controllers').controller('botController',
 			go.path('correspondentDevices.correspondentDevice');
 		});
 	}
-}).directive('dynamic', function ($compile) {
-	return {
-		restrict: 'A',
-		replace: true,
-		link: function (scope, ele, attrs) {
-			scope.$watch(attrs.dynamic, function(html) {
-				ele.html(html);
-				$compile(ele.contents())(scope);
-			});
-		}
-	};
 });
