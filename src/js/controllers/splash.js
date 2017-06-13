5a3f8802c0e2df6a5459846947bcfc782e905bbb
kakysha <kakysha@me.com>
2017-06-13 21:35:29 +0300

    disclaimer fix

--- src/js/controllers/splash.js
+++ src/js/controllers/splash.js
@@ -69,7 +69,7 @@ angular.module('copayApp.controllers').controller('splashController',
 
 	this.init = function() {
 		storageService.getDisclaimerFlag(function(err, val) {
-			if (!val) go.path('preferencesGlobal.about.disclaimer');
+			if (!val) go.path('preferencesGlobal.preferencesAbout.disclaimer');
 
 			if (profileService.profile) {
 				go.walletHome();
