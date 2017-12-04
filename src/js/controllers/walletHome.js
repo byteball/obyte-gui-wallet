06ce40deb354c7da5013ca48b1a5dbbad685bb2a
kakysha <kakysha@me.com>
2017-12-04 21:39:16 +0300

    fixes

--- src/js/controllers/walletHome.js
+++ src/js/controllers/walletHome.js
@@ -45,7 +45,7 @@ angular.module('copayApp.controllers')
 			self.setForm(address, amount, null, asset, recipient_device_address);
 
 			var form = $scope.sendPaymentForm;
-			if (form.address.$invalid && !self.blockUx) {
+			if (form.address && form.address.$invalid && !self.blockUx) {
 				console.log("invalid address, resetting form");
 				self.resetForm();
 				self.error = gettext('Could not recognize a valid Byteball QR Code');
