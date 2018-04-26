19ab65598b20584522cda1b655ae7f20daaea539
kakysha <kakysha@me.com>
2018-04-26 19:10:10 +0300

    claiming process name

--- src/js/services/go.js
+++ src/js/services/go.js
@@ -152,7 +152,9 @@ angular.module('copayApp.services').factory('go', function($window, $rootScope, 
 
 	function handleFile(uri) {
 		console.log("handleFile "+uri);
+		$rootScope.$emit('process_status_change', 'claiming', true);
 		var cb = function(data) {
+			$rootScope.$emit('process_status_change', 'claiming', false);
 			$rootScope.$emit('claimTextcoin', data.mnemonic);
 		}
 		if (uri.indexOf("content:") !== -1) {
