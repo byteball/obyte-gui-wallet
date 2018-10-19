4ace74cc5a9a658a48ab5db36d16850406068d54
Evgeniy Troynov <troinof@gmail.com>
2018-10-19 19:53:54 +0300

    #contact-filters. add filter new, asc

--- src/js/controllers/correspondentDevices.js
+++ src/js/controllers/correspondentDevices.js
@@ -1,149 +1,202 @@
 'use strict';
 
-angular.module('copayApp.controllers').controller('correspondentDevicesController',
-  function($scope, $timeout, configService, profileService, go, correspondentListService, $state, $rootScope) {
-	var wallet = require('byteballcore/wallet.js');
-	var bots = require('byteballcore/bots.js');
-	var mutex = require('byteballcore/mutex.js');
-	var fc = profileService.focusedClient;
+angular
+	.module('copayApp.controllers')
+	.controller('correspondentDevicesController', function(
+		$scope,
+		$timeout,
+		configService,
+		profileService,
+		go,
+		correspondentListService,
+		$state,
+		$rootScope
+	) {
+		var wallet = require('byteballcore/wallet.js');
+		var bots = require('byteballcore/bots.js');
+		var mutex = require('byteballcore/mutex.js');
+		var fc = profileService.focusedClient;
 
-	$scope.editCorrespondentList = false;
-	$scope.selectedCorrespondentList = {};
-	$scope.backgroundColor = fc.backgroundColor;
-
-	// Contacts filter
-	$scope.contactsSearchText  = '';
-	$scope.contactsFilter      = 'default';
-	$scope.contactsFilterLabel = 'default';
-	$scope.contactsFiltersList = ['default', 'new', 'asc'];
-
-	// Bots filter
-	$scope.botsSearchText  = '';
-	$scope.botsFilter      = 'default';
-	$scope.botsFilterLabel = 'default';
-	$scope.botsFiltersList = ['default', 'new', 'asc'];
-
-
-	$scope.state = $state;
-
-	$scope.hideRemove = true;
-
-	var listScrollTop = 0;
-
-	$scope.$on('$stateChangeStart', function(evt, toState, toParams, fromState) {
-	    if (toState.name === 'correspondentDevices') {
-	        $scope.readList();
-	    	setTimeout(function(){document.querySelector('[ui-view=chat]').scrollTop = listScrollTop;$rootScope.$emit('Local/SetTab', 'chat', true);}, 5);
-	    }
-	});
-
-	$scope.showCorrespondent = function(correspondent) {
-		console.log("showCorrespondent", correspondent);
-		correspondentListService.currentCorrespondent = correspondent;
-		listScrollTop = document.querySelector('[ui-view=chat]').scrollTop;
-		go.path('correspondentDevices.correspondentDevice');
-	};
-
-	$scope.showBot = function(bot) {
-		$state.go('correspondentDevices.bot', {id: bot.id});
-	};
-
-	$scope.toggleEditCorrespondentList = function() {
-		$scope.editCorrespondentList = !$scope.editCorrespondentList;
+		$scope.editCorrespondentList = false;
 		$scope.selectedCorrespondentList = {};
-	};
+		$scope.backgroundColor = fc.backgroundColor;
 
-	$scope.toggleSelectCorrespondentList = function(addr) {
-		$scope.selectedCorrespondentList[addr] = $scope.selectedCorrespondentList[addr] ? false : true;
-	};
+		// Contacts filter
+		$scope.contactsSearchText = '';
+		$scope.contactsFilter = 'default';
+		$scope.contactsFilterLabel = 'default';
+		$scope.contactsFiltersList = ['default', 'asc', 'new'];
+		$scope.contactsFilterHash  = {
+			default: $scope.newMsgByAddressComparator,
+			asc: [],
+			new: ''
+		}
 
-	$scope.newMsgByAddressComparator = function(correspondent) {
-	      return (-$scope.newMessagesCount[correspondent.device_address]||correspondent.name.toLowerCase());
-	};
+		// Bots filter
+		$scope.botsSearchText = '';
+		$scope.botsFilter = 'default';
+		$scope.botsFilterLabel = 'default';
+		$scope.botsFiltersList = ['default', 'asc', 'new'];
+		$scope.botsFilterHash  = {
+			default: 'newMsgByAddressComparator',
+			asc: [],
+			new: ''
+		}
 
-	$scope.beginAddCorrespondent = function() {
-		console.log("beginAddCorrespondent");
-		listScrollTop = document.querySelector('[ui-view=chat]').scrollTop;
-		go.path('correspondentDevices.addCorrespondentDevice');
-	};
+		$scope.state = $state;
 
-	$scope.changeFilter = function(filter, label) {
-		$scope[filter + 'FilterLabel'] = label;
-		$scope[filter + 'Filter'] = label;
-	}
+		$scope.hideRemove = true;
 
+		var listScrollTop = 0;
 
-	$scope.readList = function() {
-		$scope.error = null;
-		correspondentListService.list(function(err, ab) {
-			if (err) {
-				$scope.error = err;
-				return;
-			}
-
-			wallet.readDeviceAddressesUsedInSigningPaths(function(arrNotRemovableDeviceAddresses) {
-
-				// add a new property indicating whether the device can be removed or not
-				
-				var length = ab.length;
-				for (var i = 0; i < length; i++) {
- 				 	corrDev = ab[i];
-
-				 	corrDevAddr = corrDev.device_address;
-					
-				 	var ix = arrNotRemovableDeviceAddresses.indexOf(corrDevAddr);
-					
-					// device is removable when not in list
-				 	corrDev.removable = (ix == -1);
-				}
-			});
-		
-			$scope.list = ab;
-
-			bots.load(function(err, rows){
-				if (err) $scope.botsError = err.toString();
-				$scope.bots = rows;
-				$timeout(function(){
-					$scope.$digest();
-				});
-			});
-		});
-	};
-	
-	$scope.hideRemoveButton = function(removable){
-		return $scope.hideRemove || !removable;
-	}
-
-	$scope.remove = function(device_address) {
-		mutex.lock(["remove_device"], function(unlock){
-			// check to be safe
-			wallet.determineIfDeviceCanBeRemoved(device_address, function(bRemovable) {
-				if (!bRemovable) {
-					unlock();
-					return console.log('device '+device_address+' is not removable');
-				}
-				var device = require('byteballcore/device.js');
-
-				// send message to paired device
-				// this must be done before removing the device
-				device.sendMessageToDevice(device_address, "removed_paired_device", "removed");
-
-				// remove device
-				device.removeCorrespondentDevice(device_address, function() {
-					unlock();
-					$scope.hideRemove = true;
-					correspondentListService.currentCorrespondent = null;
-					$scope.readList();
+		$scope.$on('$stateChangeStart', function(
+			evt,
+			toState,
+			toParams,
+			fromState
+		) {
+			if (toState.name === 'correspondentDevices') {
+				$scope.readList();
+				setTimeout(function() {
+					document.querySelector(
+						'[ui-view=chat]'
+					).scrollTop = listScrollTop;
 					$rootScope.$emit('Local/SetTab', 'chat', true);
-					setTimeout(function(){document.querySelector('[ui-view=chat]').scrollTop = listScrollTop;}, 5);
+				}, 5);
+			}
+		});
+
+		$scope.showCorrespondent = function(correspondent) {
+			console.log('showCorrespondent', correspondent);
+			correspondentListService.currentCorrespondent = correspondent;
+			listScrollTop = document.querySelector('[ui-view=chat]').scrollTop;
+			go.path('correspondentDevices.correspondentDevice');
+		};
+
+		$scope.showBot = function(bot) {
+			$state.go('correspondentDevices.bot', { id: bot.id });
+		};
+
+		$scope.toggleEditCorrespondentList = function() {
+			$scope.editCorrespondentList = !$scope.editCorrespondentList;
+			$scope.selectedCorrespondentList = {};
+		};
+
+		$scope.toggleSelectCorrespondentList = function(addr) {
+			$scope.selectedCorrespondentList[addr] = $scope
+				.selectedCorrespondentList[addr]
+				? false
+				: true;
+		};
+
+		$scope.newMsgByAddressComparator = function(correspondent) {
+			return (
+				-$scope.newMessagesCount[correspondent.device_address] ||
+				correspondent.name.toLowerCase()
+			);
+		};
+
+		$scope.beginAddCorrespondent = function() {
+			console.log('beginAddCorrespondent');
+			listScrollTop = document.querySelector('[ui-view=chat]').scrollTop;
+			go.path('correspondentDevices.addCorrespondentDevice');
+		};
+
+		$scope.changeFilter = function(filter, label) {
+			$scope[filter + 'FilterLabel'] = label;
+			$scope[filter + 'Filter'] = label;
+		};
+
+		$scope.readList = function() {
+			$scope.error = null;
+			correspondentListService.list(function(err, ab) {
+				if (err) {
+					$scope.error = err;
+					return;
+				}
+
+				console.info(wallet);
+
+				wallet.readDeviceAddressesUsedInSigningPaths(function(
+					arrNotRemovableDeviceAddresses
+				) {
+					// add a new property indicating whether the device can be removed or not
+
+					var length = ab.length;
+					for (var i = 0; i < length; i++) {
+						corrDev = ab[i];
+
+						corrDevAddr = corrDev.device_address;
+
+						var ix = arrNotRemovableDeviceAddresses.indexOf(
+							corrDevAddr
+						);
+
+						// device is removable when not in list
+						corrDev.removable = ix == -1;
+					}
+				});
+
+				$scope.list = ab;
+
+				bots.load(function(err, rows) {
+					if (err) $scope.botsError = err.toString();
+					$scope.bots = rows;
+					$timeout(function() {
+						$scope.$digest();
+					});
 				});
 			});
-		});
-	};
+		};
 
-	$scope.cancel = function() {
-		console.log("cancel clicked");
-		go.walletHome();
-	};
+		$scope.hideRemoveButton = function(removable) {
+			return $scope.hideRemove || !removable;
+		};
 
-  });
+		$scope.remove = function(device_address) {
+			mutex.lock(['remove_device'], function(unlock) {
+				// check to be safe
+				wallet.determineIfDeviceCanBeRemoved(device_address, function(
+					bRemovable
+				) {
+					if (!bRemovable) {
+						unlock();
+						return console.log(
+							'device ' + device_address + ' is not removable'
+						);
+					}
+					var device = require('byteballcore/device.js');
+
+					// send message to paired device
+					// this must be done before removing the device
+					device.sendMessageToDevice(
+						device_address,
+						'removed_paired_device',
+						'removed'
+					);
+
+					// remove device
+					device.removeCorrespondentDevice(
+						device_address,
+						function() {
+							unlock();
+							$scope.hideRemove = true;
+							correspondentListService.currentCorrespondent = null;
+							$scope.readList();
+							$rootScope.$emit('Local/SetTab', 'chat', true);
+							setTimeout(function() {
+								document.querySelector(
+									'[ui-view=chat]'
+								).scrollTop = listScrollTop;
+							}, 5);
+						}
+					);
+				});
+			});
+		};
+
+		$scope.cancel = function() {
+			console.log('cancel clicked');
+			go.walletHome();
+		};
+	});
