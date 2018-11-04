ea6084d1d4536f021860b3e0da24a17d0d8182ce
tony <tonyofbyteball@users.noreply.github.com>
2018-11-04 18:13:52 +0300

    dynamic placeholders for feed name and value and link to instructions

--- src/js/services/configService.js
+++ src/js/services/configService.js
@@ -24,27 +24,42 @@ angular.module('copayApp.services').factory('configService', function(storageSer
 
   root.oracles = {
 		"FOPUBEUPBC6YLIQDLKL6EW775BMV7YOH": {
-			name: "Bitcoin Oracle",
+			name: "Bitcoin oracle",
+			feedname_placeholder: "bitcoin_merkle or randomXXXXXX",
+			feedvalue_placeholder: "e.g. 1LR5xew1X13okNYKRu7qA3uN4hpRH1Tfnn:0.5",
+			instructions_url: "https://medium.com/byteball/making-p2p-great-again-episode-ii-bitcoin-exchange-d98adfbde2a5",
 			feednames_filter: ["^bitcoin_merkle$", "^random[\\d]+$"],
 			feedvalues_filter: ["^[13][a-km-zA-HJ-NP-Z1-9]{25,34}\\:[0-9\\.]+$", "^\\d{1,6}$"]
 		},
 		"JPQKPRI5FMTQRJF4ZZMYZYDQVRD55OTC" : {
-			name: "Crypto exchange rates",
+			name: "Crypto exchange rates oracle",
+			feedname_placeholder: "e.g. BTC_USD",
+			feedvalue_placeholder: "e.g. 1234.56",
+			instructions_url: "https://wiki.byteball.org/Oracle#Using_the_crypto-exchange-rates_oracle_in_a_smart_contract",
 			feednames_filter: ["^[\\dA-Z]+_[\\dA-Z]+$"],
 			feedvalues_filter: ["^[\\d\\.]+$"]
 		},
 		"GFK3RDAPQLLNCMQEVGGD2KCPZTLSG3HN" : {
-			name: "Flight delay tracker",
+			name: "Flight delay oracle",
+			feedname_placeholder: "e.g. BA950-2018-12-25",
+			feedvalue_placeholder: "e.g. 30",
+			instructions_url: "https://wiki.byteball.org/Oracle#Flight_delays_tracker",
 			feednames_filter: ["^[\\w\\d]+-\\d{4}-\\d{2}-\\d{2}$"],
 			feedvalues_filter: ["^[\\d]+$"]
 		},
 		"TKT4UESIKTTRALRRLWS4SENSTJX6ODCW" : {
-			name: "Sports betting",
+			name: "Sports betting oracle",
+			feedname_placeholder: "e.g. BROOKLYNNETS_CHARLOTTEHORNETS_2018-03-21",
+			feedvalue_placeholder: "e.g. BROOKLYNNETS",
+			instructions_url: "https://wiki.byteball.org/Sports_betting",
 			feednames_filter: ["^[\\w\\d]+_[\\w\\d]+_\\d{4}-\\d{2}-\\d{2}$"],
 			feedvalues_filter: ["^[\\w\\d]+$"]
 		},
 		"I2ADHGP4HL6J37NQAD73J7E5SKFIXJOT" : {
-			name: "Timestamp",
+			name: "Timestamp oracle",
+			feedname_placeholder: "timestamp",
+			feedvalue_placeholder: "e.g. 1541341626704",
+			instructions_url: "https://wiki.byteball.org/Oracle",
 			feednames_filter: ["^timestamp$"],
 			feedvalues_filter: ["^\\d{13,}$"]
 		}
