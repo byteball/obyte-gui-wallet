9d596c6b23687cc84328e170ca40611d7f1f10f6
Alexey Gerasimov <a.gerasimov@ntr1x.com>
2019-08-21 11:28:42 +0500

    suggest list fix + replace fix

--- src/js/controllers/preferencesEditWitness.js
+++ src/js/controllers/preferencesEditWitness.js
@@ -1,27 +1,28 @@
 'use strict';
 
 angular.module('copayApp.controllers').controller('preferencesEditWitnessController',
-  function($scope, $timeout, go, witnessListService) {
+  function($scope, $timeout, go, witnessListService, lodash) {
     
     var self = this;
     this.witness = witnessListService.currentWitness;
     this.witnesses = [];
-    this.witnesses2 = [];
+    this.witnessesKnown = [];
 
     this.witnessList = require('ocore/my_witnesses.js');
 
     this.witnessesAddInfo = require('ocore/network.js').knownWitnesses;
 
-    for ( var witness in this.witnessesAddInfo) {
-
-    }
+    this.witnessesKnown = Object.keys(this.witnessesAddInfo);
 
     this.witnessList.readMyWitnesses(function(arrWitnesses){
         self.witnesses = arrWitnesses;
         $timeout(function () {
             $scope.$apply();
         });
+        self.witnessesKnown = lodash.difference(lodash.difference(self.witnessesKnown, self.witnesses), lodash.difference(self.witnesses, self.witnessesKnown));
     }, 'wait');
+
+
 
 
     this.save = function() {
