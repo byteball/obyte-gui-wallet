a77a97d8fc2e4cc576457e23a73f9c64657846ce
tony <tonyofbyteball@users.noreply.github.com>
2020-06-19 21:42:47 +0300

    startsWith polyfill for android 4.4

--- src/js/polyfills.js
+++ src/js/polyfills.js
@@ -19,3 +19,12 @@ if (typeof Object.assign != 'function') {
     return to;
   };
 }
+
+if (!String.prototype.startsWith) {
+  Object.defineProperty(String.prototype, 'startsWith', {
+      value: function(search, rawPos) {
+          var pos = rawPos > 0 ? rawPos|0 : 0;
+          return this.substring(pos, pos + search.length) === search;
+      }
+  });
+}
