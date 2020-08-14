fbdac9481322262db0310591f6bded655dfe44bc
devneser <alexzh.dev@gmail.com>
2020-08-14 22:23:18 +0100

    update ui

--- src/js/controllers/walletHome.js
+++ src/js/controllers/walletHome.js
@@ -7,7 +7,7 @@ var ValidationUtils = require('ocore/validation_utils.js');
 var parse_ojson = require('ocore/formula/parse_ojson');
 
 angular.module('copayApp.controllers')
-	.controller('walletHomeController', function($scope, $rootScope, $timeout, $filter, $modal, $log, notification, isCordova, profileService, lodash, configService, storageService, gettext, gettextCatalog, nodeWebkit, addressService, confirmDialog, animationService, addressbookService, correspondentListService, newVersion, autoUpdatingWitnessesList, go, aliasValidationService) {
+	.controller('walletHomeController', function($scope, $rootScope, $timeout, $filter, $modal, $log, notification, isCordova, profileService, lodash, configService, storageService, gettext, gettextCatalog, nodeWebkit, addressService, confirmDialog, animationService, addressbookService, correspondentListService, newVersion, autoUpdatingWitnessesList, go, aliasValidationService, fileSystemService) {
 
 		var self = this;
 		var home = this;
@@ -2349,9 +2349,11 @@ angular.module('copayApp.controllers')
 			else if (isCordova)
 				cordova.InAppBrowser.open(url, '_system');
 		};
+
 		this.sendAttachedFile = function ($ev) {
 			home.attachedFile = $ev.target.files[0];
-			if (!home.attachedFile) return;
+			if (!home.attachedFile) 
+				return;
 			fileSystemService.readFile(home.attachedFile.path, function (
 				err,
 				data
@@ -2368,6 +2370,7 @@ angular.module('copayApp.controllers')
 				$scope.$apply();
 			});
 		};
+
 		this.openTxModal = function(btx) {
 			$rootScope.modalOpened = true;
 			delete $rootScope.newPaymentsCount[btx.unit];
