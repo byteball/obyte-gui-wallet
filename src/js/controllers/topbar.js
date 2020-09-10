22251777549184a8e134fa64e613b41fdc94e978
devneser <alexzh.dev@gmail.com>
2020-09-10 22:16:14 +0400

    feat: update wallet ui #8

--- src/js/controllers/topbar.js
+++ src/js/controllers/topbar.js
@@ -3,10 +3,9 @@
 angular.module('copayApp.controllers').controller('topbarController', function($scope, $rootScope, go) {
     const walletSettings = ['Preferences', 'Wallet Alias', 'Color', 'Hidden assets', 'Hidden subwallets', 'Advanced', 'Wallet Information', 'Sweep paper wallet', 'Delete Wallet'];
 
-    this.showableWalletAvatar = function(title) {
-        if (walletSettings.indexOf(title) !== -1)
-            return true;
-        return false;
+
+    this.showableWalletAvatar = function() {
+        return $rootScope.showableWalletAvatar;
     }
 
     this.onQrCodeScanned = function(data) {
