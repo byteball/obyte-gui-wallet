b358ca9773a75d4e4867e3429d66913c07680ac0
tony <tonyofbyteball@users.noreply.github.com>
2020-10-09 02:10:32 +0300

    polyfill for closest()

--- src/js/polyfills.js
+++ src/js/polyfills.js
@@ -120,3 +120,22 @@ if (!Array.prototype.findIndex) {
     writable: true
   });
 }
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+if (!Element.prototype.matches) {
+  Element.prototype.matches =
+    Element.prototype.msMatchesSelector || 
+    Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function(s) {
+    var el = this;
+
+    do {
+      if (Element.prototype.matches.call(el, s)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+  };
+}
