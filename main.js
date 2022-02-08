551ef5a67c5b3748c43850a48acec0ab147cbeca
kakysha <kakysha@me.com>
2022-02-08 15:33:14 +0300

    enable copy-paste

--- main.js
+++ main.js
@@ -185,6 +185,17 @@ app.whenReady().then(() => {
 		const template = [{
 				label: app.name,
 				submenu:[{label: 'Quit', role: 'quit'}]
+		}, {
+			label: 'Edit',
+			submenu: [
+				{label: 'Undo', accelerator: 'CmdOrCtrl+Z', role: 'undo'},
+				{label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', role: 'redo'},
+				{type: 'separator'},
+				{label: 'Cut', accelerator: 'CmdOrCtrl+X', role: 'cut'},
+				{label: 'Copy', accelerator: 'CmdOrCtrl+C', role: 'copy'},
+				{label: 'Paste', accelerator: 'CmdOrCtrl+V', role: 'paste'},
+				{label: 'Select All', accelerator: 'CmdOrCtrl+A', role: 'selectAll'}
+			]
 		}];
 		const menu = Menu.buildFromTemplate(template);
 		Menu.setApplicationMenu(menu);
